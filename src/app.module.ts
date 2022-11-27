be4bddd2af4a957ffcfdc4c31313958b84ec4e8f
LucasSSDK <l_ucass_oares@outlook.com>
2022-11-27 19:20:34 -0300

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://Lucas:<Lucas>@news.h8f4ivx.mongodb.net/test')],
   controllers: [],
   providers: [],
 })
