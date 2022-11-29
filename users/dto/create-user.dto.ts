166ef1d8996d4377efa47a72c39100096a7a6d59
LucasSSDK <l_ucass_oares@outlook.com>
2022-11-29 00:03:03 -0300

    update

--- users/dto/create-user.dto.ts
+++ users/dto/create-user.dto.ts
@@ -3,14 +3,18 @@ import { IsEmail, IsNotEmpty } from 'class-validator';
 export class UserDto {
   @ApiProperty()
   name: string;
+
   @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
+
   @ApiProperty()
   password: string;
+
   @ApiProperty()
   cpf: string;
+  
   @ApiProperty()
   role: string;
 }
