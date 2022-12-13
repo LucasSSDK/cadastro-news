7eeb0250f3fb404498f4191d3095254b150102e3
LucasSSDK <l_ucass_oares@outlook.com>
2022-12-13 19:37:41 -0300

    feat: create employee entity

--- src/Employees/entities/employee.entity.ts
+++ src/Employees/entities/employee.entity.ts
@@ -1,1 +1,11 @@
-export class Employee {}
+import { EmployeeDto } from "../dto/create-employee.dto";
+
+export interface IEmployeeEntity extends EmployeeDto {
+    id: string;
+    name: string;
+    idade: number;
+    cpf: string;
+    email: string;
+    password: string;
+    role: string;
+  }
